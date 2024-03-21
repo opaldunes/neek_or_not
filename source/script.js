1f0a41d7bbc6103a99bf905e587e56d3f811b61d
miguelhamdi <miguelhamdi@gmail.com>
2024-03-21 20:21:48 +0100

    log in page & other small edits

--- source/script.js
+++ source/script.js
@@ -1,3 +1,32 @@
+
+
+// Log-in 
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get the username and password from the form
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var isAuthenticated = false;
+
+    // Check if the username and password are valid
+    if ((username === "miguel" || username === "monty") && password === "brawl") {
+        isAuthenticated = true;
+    }
+
+    if (isAuthenticated) {
+        // Hide login form
+        document.getElementById("login-container").style.display = "none";
+        // Display main content
+        document.getElementById("main-content").style.display = "block";
+    } else {
+        // Display error message or handle unsuccessful login
+        alert("Invalid username or password. Please try again.");
+    }
+});
+
+// Main Page
 const onlineBtn = document.getElementById('onlineBtn');
 const workBtn = document.getElementById('workBtn');
 
