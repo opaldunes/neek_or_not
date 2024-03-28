0ee736f9d9d4281294de357f9424284d22b00a9a
miguelhamdi <miguelhamdi@gmail.com>
2024-03-28 17:34:48 +0100

    assa

--- source/script.js
+++ source/script.js
@@ -1,5 +1,6 @@
 const onlineBtn = document.getElementById('onlineBtn');
 const workBtn = document.getElementById('workBtn');
+const mainContent = document.getElementById('main-content');
 
 const apiKey = 'patIfaS9rxvey5yzF.ffb1ac6f25310d8d49cb12ce82ed0020effde4922d6dd636d513145d59ead35a';
 const url = 'https://api.airtable.com/v0/appACEYultgfTJ5MV/button-status';
@@ -7,7 +8,7 @@ const url = 'https://api.airtable.com/v0/appACEYultgfTJ5MV/button-status';
 // Function to handle button click
 function handleClick(event) {
     const clickedBtnId = event.target.id;
-    
+
     // Update button classes
     if (clickedBtnId === 'onlineBtn') {
         console.log('online');
@@ -26,75 +27,86 @@ function handleClick(event) {
 }
 
 function updateButtonStatus(newStatus) {
-    const url_update = url + '/rec5FOGLqoW9e7SVg'
+    const url_update = url + '/rec5FOGLqoW9e7SVg';
 
     const requestData = {
-      fields: {
-        Name: newStatus
-      }
+        fields: {
+            Name: newStatus
+        }
     };
-    
+
     fetch(url_update, {
-      method: 'PATCH',
-      headers: {
-        'Authorization': `Bearer ${apiKey}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(requestData)
+        method: 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${apiKey}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
     })
     .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
     })
     .then(data => {
-      console.log('Button status updated:', data.fields.Name);
+        console.log('Button status updated:', data.fields.Name);
     })
     .catch(error => {
-      console.error('Error:', error);
+        console.error('Error:', error);
     });
 }
 
 async function fetchButtonStatus() {
     try {
-      const response = await fetch(url, {
-        headers: {
-          'Authorization': `Bearer ${apiKey}`
-        }
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        const record = data.records.find(record => record.id === "rec5FOGLqoW9e7SVg");
-        console.log('Record:', record.fields.Name)
-        return record ? record.fields.Name : null;
-      } else {
-        console.error('Failed to fetch button status:', response.statusText);
+        const response = await fetch(url, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            const record = data.records.find(record => record.id === "rec5FOGLqoW9e7SVg");
+            console.log('Record:', record.fields.Name)
+            return record ? record.fields.Name : null;
+        } else {
+            console.error('Failed to fetch button status:', response.statusText);
         }
     } catch (error) {
-      console.error('Error:', error.message);
+        console.error('Error:', error.message);
     }
 }
 
 async function main() {
     const button_status = await fetchButtonStatus();
 
-    // Add event listeners
-    onlineBtn.addEventListener('click', handleClick);
-    workBtn.addEventListener('click', handleClick);
+    // Check if user is authenticated
+    auth0.createAuth0Client({
+        domain: "dev-q2uoeptszv4xfljl.eu.auth0.com",
+        clientId: "XUzajVRXoDxADinp4Xz6BdZJlDlC6yJH",
+        authorizationParams: {
+            redirect_uri: window.location.origin + "/index.html"
+        }
+    }).then(async (auth0Client) => {
+        const isAuthenticated = await auth0Client.isAuthenticated();
+        if (isAuthenticated) {
+            // Show main content
+            mainContent.style.display = "block";
+        }
 
-    // Check if there's a previously selected button
-    const selectedButtonId = localStorage.getItem('selectedButton');
-    console.log(button_status)
-    if (button_status) {
-        // Simulate click on the previously selected button
-        document.getElementById(button_status).click();
-    }
+        // Add event listeners
+        onlineBtn.addEventListener('click', handleClick);
+        workBtn.addEventListener('click', handleClick);
+
+        // Check if there's a previously selected button
+        if (button_status) {
+            // Simulate click on the previously selected button
+            document.getElementById(button_status).click();
+        }
+    });
 }
-  
-    main().catch(error => {
+
+main().catch(error => {
     console.error('An error occurred:', error);
 });
-
-
